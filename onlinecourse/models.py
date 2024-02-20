e595993abcf0c00becaf1a3c4746348ba8494ec8
charles <mo.charles2019@gmail.com>
2024-02-20 15:03:30 -0500

    change placeholder in templates and change 'correct' to 'is_correct' in choice model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -114,7 +114,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
-    correct = models.BooleanField(default=False)
+    is_correct = models.BooleanField(default=False)
 
 class Submission(models.Model):
    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
