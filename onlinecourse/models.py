fa5d81e855118a17b6afaa9356a336cf4f8c3924
charles <mo.charles2019@gmail.com>
2024-02-20 10:34:56 -0500

    import new models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -116,10 +116,7 @@ class Choice(models.Model):
     content = models.CharField(max_length=200)
     correct = models.BooleanField(default=False)
 
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
 class Submission(models.Model):
    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
    choices = models.ManyToManyField(Choice)
-   
+
