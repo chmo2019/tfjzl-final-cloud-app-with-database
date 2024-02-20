325c38f3c8ba795d664627983a8ac1b11edc9cbc
charles <mo.charles2019@gmail.com>
2024-02-20 10:20:50 -0500

    add choice model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -111,6 +111,11 @@ class Question(models.Model):
         else:
             return False
 
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
+
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
